4e9ad412792d7a8a95ccb18b127d775a0dc21a97
Droomii <dowoo2594@naver.com>
2020-03-01 14:59:43 +0900

    working on crossword class

--- EngPRJ/src/poly/util/Crossword.java
+++ EngPRJ/src/poly/util/Crossword.java
@@ -7,16 +7,18 @@ import java.util.List;
 public class Crossword {
 	
 	private List<String> wordsList;
-	private List<List<String>> matrix;
+	private List<List<Character>> matrix;
+	int colSize = 0;
+	int rowSize = 0;
 	
 	public Crossword(String...strings) {
 		this.wordsList = Arrays.asList(strings);
 		this.matrix = new ArrayList<>();
 		
 		for (int i = 0; i < 5; i++) {
-			List<String> row = new ArrayList<>();
+			List<Character> row = new ArrayList<>();
 			for (int j = 0; j < 5; j++) {
-				row.add("-");
+				row.add('-');
 			}
 			this.matrix.add(row);
 		}
@@ -32,20 +34,11 @@ public class Crossword {
 	}
 
 
-	public List<List<String>> getMatrix() {
-		return matrix;
-	}
-
-
-	public void setMatrix(List<List<String>> matrix) {
-		this.matrix = matrix;
-	}
-
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		for(List<String> row : matrix) {
-			for(String s : row) {
+		for(List<Character> row : matrix) {
+			for(char s : row) {
 				sb.append(s);
 			}
 			sb.append("\n");
@@ -55,20 +48,33 @@ public class Crossword {
 	}
 	
 	public void addCol() {
-		for(List<String> row : matrix) {
-			row.add("-");
+		for(List<Character> row : matrix) {
+			row.add('-');
 		}
 	}
 	
 	public void addRow() {
-		List<String> row = new ArrayList<>();
+		List<Character> row = new ArrayList<>();
 		
 		for(int i = 0; i < matrix.get(0).size(); i++)
-			row.add("-");
-		
+			row.add('-');
 		this.matrix.add(row);
 	}
 	
+	/**
+	 * 행, 열에 위치한 문자를 출력해줌
+	 * @param row : 행
+	 * @param col : 열
+	 * @return
+	 */
+	public char get(int row, int col) {
+		return this.matrix.get(row).get(col);
+	}
+	
+	public void set(int row, int col, char pChar) {
+		pChar = Character.toUpperCase(pChar);
+		this.matrix.get(row).set(col, pChar);
+	}
 
 	public static void main(String[] args) {
 		Crossword cw = new Crossword("hello", "name");
@@ -76,6 +82,8 @@ public class Crossword {
 		cw.addCol();
 		System.out.println(cw);
 		cw.addRow();
+		System.out.println(cw);
+		cw.set(1, 2, 'a');
 		System.out.println(cw);
 	}
 }
