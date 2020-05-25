e1cbfdb7773f280d1547ed1c460a800151f20dff
Droomii <dowoo2594@naver.com>
2020-05-25 14:56:57 +0900

    added translate() method to NewsDTO

--- EngPRJ/src/poly/dto/NewsDTO.java
+++ EngPRJ/src/poly/dto/NewsDTO.java
@@ -24,46 +24,46 @@ public class NewsDTO {
 	private Date insertDate;
 	private String newsUrl;
 
-	public NewsDTO() {}
-	
+	public NewsDTO() {
+	}
+
 	public NewsDTO(String newsTitle, CoreDocument doc, String newsUrl) throws Exception {
-		
+
 		this.newsTitle = newsTitle;
 		this.newsUrl = newsUrl;
-		
+
 		// attributes initialization
 		this.tokens = new ArrayList<List<String>>();
 		this.pos = new ArrayList<List<String>>();
 		this.originalSentence = new ArrayList<String>();
 		this.lemmas = new ArrayList<List<String>>();
-		
-		
+
 		// iterating through sentence
-	    for(Iterator<CoreSentence> it = doc.sentences().iterator(); it.hasNext();) {
-	    	CoreSentence sentence = it.next();
-	    	originalSentence.add(sentence.text());
-	    	
-	    	// adding tokens, lemmas by sentence
-	    	List<String> token = new ArrayList<String>();
-	    	List<String> lemma = new ArrayList<String>();
-	    	List<String> posBySent = new ArrayList<String>();
-	    	for(Iterator<CoreLabel> tokenIt = sentence.tokens().iterator();tokenIt.hasNext();) {
-	    		CoreLabel tempToken = tokenIt.next();
-	    		token.add(tempToken.originalText());
-	    		lemma.add(tempToken.lemma());
-	    	}
-	    	
-	    	// adding pos tags by sentence
-	    	posBySent.addAll(sentence.posTags());
-	    	
-	    	// adding nlp data split by sentence
-	    	tokens.add(token);
-	    	pos.add(posBySent);
-	    	lemmas.add(lemma);
-	    }
-	    this.insertDate = new Date();
+		for (Iterator<CoreSentence> it = doc.sentences().iterator(); it.hasNext();) {
+			CoreSentence sentence = it.next();
+			originalSentence.add(sentence.text());
+
+			// adding tokens, lemmas by sentence
+			List<String> token = new ArrayList<String>();
+			List<String> lemma = new ArrayList<String>();
+			List<String> posBySent = new ArrayList<String>();
+			for (Iterator<CoreLabel> tokenIt = sentence.tokens().iterator(); tokenIt.hasNext();) {
+				CoreLabel tempToken = tokenIt.next();
+				token.add(tempToken.originalText());
+				lemma.add(tempToken.lemma());
+			}
+
+			// adding pos tags by sentence
+			posBySent.addAll(sentence.posTags());
+
+			// adding nlp data split by sentence
+			tokens.add(token);
+			pos.add(posBySent);
+			lemmas.add(lemma);
+		}
+		this.insertDate = new Date();
 	}
-	
+
 	public void translate() throws Exception {
 		this.translation = TranslateUtil.translateNews(this.originalSentence);
 	}
@@ -71,13 +71,14 @@ public class NewsDTO {
 	@SuppressWarnings("unchecked")
 	public NewsDTO(DBObject firstNews) {
 
-		newsTitle = nvl((String) firstNews.get("newsTitle"));
-		originalSentence = (List<String>) firstNews.get("originalSentence");
-		tokens = (List<List<String>>) firstNews.get("tokens");
-		lemmas = (List<List<String>>) firstNews.get("lemmas");
-		pos = (List<List<String>>) firstNews.get("pos");
-		insertDate = (Date) firstNews.get("insertDate");
-		newsUrl = nvl((String) firstNews.get("newsUrl"));
+		this.newsTitle = nvl((String) firstNews.get("newsTitle"));
+		this.originalSentence = (List<String>) firstNews.get("originalSentence");
+		this.translation = (List<String>) firstNews.get("translation");
+		this.tokens = (List<List<String>>) firstNews.get("tokens");
+		this.lemmas = (List<List<String>>) firstNews.get("lemmas");
+		this.pos = (List<List<String>>) firstNews.get("pos");
+		this.insertDate = (Date) firstNews.get("insertDate");
+		this.newsUrl = nvl((String) firstNews.get("newsUrl"));
 	}
 
 	public List<List<String>> getLemmas() {
@@ -95,8 +96,6 @@ public class NewsDTO {
 	public void setOriginalSentence(List<String> originalSentence) {
 		this.originalSentence = originalSentence;
 	}
-	
-	
 
 	public List<String> getTranslation() {
 		return translation;
@@ -145,7 +144,5 @@ public class NewsDTO {
 	public void setNewsUrl(String newsUrl) {
 		this.newsUrl = newsUrl;
 	}
-	
-	
-	
+
 }
