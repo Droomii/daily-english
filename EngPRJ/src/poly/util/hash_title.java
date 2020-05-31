7770221144522807cde0c2a069483594ce758782
Droomii <dowoo2594@naver.com>
2020-05-31 18:46:12 +0900

    hash title reformat

--- EngPRJ/src/poly/util/hash_title.java
+++ EngPRJ/src/poly/util/hash_title.java
@@ -2,25 +2,19 @@ package poly.util;
 
 import java.security.MessageDigest;
 
-public class hash_title 
-{
-	String hash_start(String str)
-	{
-		String SHA="";
-		try 
-		{
+public class hash_title {
+	String hash_start(String str) {
+		String SHA = "";
+		try {
 			MessageDigest sh = MessageDigest.getInstance("SHA-256");
 			sh.update(str.getBytes());
 			byte byteData[] = sh.digest();
 			StringBuffer sb = new StringBuffer();
-			for(int i=0;i<byteData.length; i++)
-			{
-				sb.append(Integer.toString((byteData[i]&0xff)+0x100, 16).substring(1));
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
 			}
 			SHA = sb.toString();
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return SHA;
