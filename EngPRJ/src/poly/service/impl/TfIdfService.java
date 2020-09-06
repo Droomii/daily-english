b56d5d01464920f0ba0fcbdcfb814135002d111e
Droomii <dowoo2594@naver.com>
2020-09-07 00:32:44 +0900

    200907 tf-idf wip

--- EngPRJ/src/poly/service/impl/TfIdfService.java
+++ EngPRJ/src/poly/service/impl/TfIdfService.java
@@ -1,13 +1,10 @@
 package poly.service.impl;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.annotation.Resource;
 
@@ -34,6 +31,7 @@ public class TfIdfService implements ITfIdfService {
 	public List<Map<String, Double>> getAllArticles() throws Exception {
 		List<NewsDTO> articles = mongoNewsMapper.getAllArticles();
 
+		int docCnt = articles.size();
 		// 기사별 단어 빈도수 계산
 		List<Map<String, Integer>> wordCount = articles.stream().map(a -> {
 			Map<String, Integer> wc = new HashMap<String, Integer>();
@@ -55,18 +53,32 @@ public class TfIdfService implements ITfIdfService {
 			a.forEach((k, v)->{
 				tfMap.put(k, v.doubleValue()/wordCnt);
 			});
-			tfMap.entrySet().stream().sorted((o1, o2)-> Double.compare(o2.getValue(), o1.getValue())).limit(tfMap.size()-10).forEach(e ->tfMap.remove(e.getKey())
-			);
 			return tfMap;
 		}).collect(Collectors.toList());
 		
-		// IDF 계산
-		
-		
-		Map<String, Integer> wordInDocCnt = new HashMap<>();
+		// Document Frequency
+		Map<String, Integer> df = new HashMap<>();
 		wordCount.stream().flatMap(a -> a.entrySet().stream()).forEach(a -> {
-			wordInDocCnt.compute(a.getKey(), (k, v) -> (v == null) ? a.getValue() : v + a.getValue());
+			df.compute(a.getKey(), (k, v) -> (v == null) ? 1 : v + 1);
 		});
-		return tf;
+		
+		// IDF
+		Map<String, Double> idf = new HashMap<>();
+		df.forEach((k, v)->{
+			idf.put(k, Math.log(docCnt / v.doubleValue()));
+		});
+		
+		// TF-IDF
+		List<Map<String, Double>> tfIdf = tf.stream().map(a ->{
+			Map<String, Double> tfIdfMap = new HashMap<>();
+			a.forEach((k, v)->{
+				tfIdfMap.put(k, v * idf.get(k));
+			});
+			tfIdfMap.entrySet().stream().sorted((o1, o2)->Double.compare(o1.getValue(), o2.getValue())).limit(tfIdfMap.size()-20).forEach(kv ->{
+				tfIdfMap.remove(kv.getKey());
+			});
+			return tfIdfMap;	
+		}).collect(Collectors.toList());
+		return tfIdf;
 	}
 }
